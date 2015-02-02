67d48320d6161e94ef91fda2823271087baccd15
Marcus Cripps <marcuscripps@hotmail.com>
2015-02-02 20:30:37 +0000

    Comp4711 tutorial 2c Signed-off-by:Marcus Cripps <marcuscripps@hotmail.com>

---
+++ application/models/Images.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Images
+ *
+ * @author Marcus
+ */
+class Images extends CI_Model{
+    function _construct()
+    {
+        parent::_construct();
+    }
+    
+    // return all of the images, descending order by the date posted
+    function all()
+    {
+        $this->db->order_by("id", "desc");
+        $query = $this->db->get('images');
+        return $query->result_array();
+    }
+    // return the three most recently posted images
+    function newest()
+    {
+        $this->db->order_by("id", "desc");
+        $this->db->limit(3);
+        $query = $this->db->get('images');
+        return $query->result_array();
+    }
+}
